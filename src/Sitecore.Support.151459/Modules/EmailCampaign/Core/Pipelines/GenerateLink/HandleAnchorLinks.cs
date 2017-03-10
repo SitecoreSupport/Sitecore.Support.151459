ee647068f8be679908c749fbc649bf32294ef692
Dmitry Baranov <dbr@sitecore.net>
2017-03-10 12:55:01 +0200

    Anchor links href property is stripped out.

---
+++ src/Sitecore.Support.151459/Modules/EmailCampaign/Core/Pipelines/GenerateLink/HandleAnchorLinks.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Core.Extensions;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink;
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Web;
+
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink
+{ 
+    public class HandleAnchorLinks : GenerateLinkProcessor
+    {
+        public override void Process(GenerateLinkPipelineArgs args)
+        {
+            Assert.IsNotNull(args, "Arguments can't be null");
+            if (args.Url.StartsWith("/#"))
+            {
+                args.GeneratedUrl = args.Url.Remove(0, 1);                
+            }
+        }
+    }
+}
